8cbdc79470ad47eb0f141d0bb6931423d75d563a
Yash Vishwas <denhim640@gmail.com>
2026-07-14 19:30:19 +0530

    Implement database migrations with Flask-Migrate

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,12 +4,12 @@ from app.routes.auth import auth_bp
 from app.utils.error_handlers import register_error_handlers
 from dotenv import load_dotenv
 from app.config import Config
-from app.database.db import db
+from app.database.db import db, migrate
 from app.utils.logger import logger
-from app.models.user import User
 from app.utils.jwt_manager import jwt
 
 load_dotenv()
+
 
 def create_app():
     app = Flask(__name__)
@@ -17,10 +17,9 @@ def create_app():
     app.config.from_object(Config)
 
     db.init_app(app)
+    migrate.init_app(app, db)
     jwt.init_app(app)
-    with app.app_context():
-        db.create_all()
-        logger.info("Database tables created successfully")
+
     logger.info("Database initialized successfully")
 
     app.register_blueprint(health_bp)
